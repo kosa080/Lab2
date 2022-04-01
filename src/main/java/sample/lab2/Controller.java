ee289054a51faebe7db796ad501ceef09a47d1c4
Maciej Kosakowski <kosakowski222@gmail.com>
2022-04-01 12:38:04 +0200

    Punkt 5, Dodanie pola edycji tekstu z identyfikatorem fx:id txtLicznik, który wyświetla liczbę kliknięć.

--- src/main/java/sample/lab2/Controller.java
+++ src/main/java/sample/lab2/Controller.java
@@ -3,15 +3,21 @@ package sample.lab2;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 
 public class Controller {
     public Button btnMain;
+    public TextField txtLicznik;
     int clickCounter = 0;
 
     public void onBtnAction(ActionEvent actionEvent) {
         System.out.println("Klikniecie nr " + ++clickCounter);
         if (clickCounter == 1)
             btnMain.setText("I jeszcze raz");
+    }
+
+    public void onTxt(ActionEvent actionEvent) {
+            txtLicznik.setText(txtLicznik.getText()+ clickCounter);
     }
 }
